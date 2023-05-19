ec86336e8ae902def906b865b68c93eea36fc963
Harri Rissanen <=>
2023-05-19 13:37:48 +0300

    Use the new API key property in tests too

--- src/test/java/com/github/harriris/wdapi/WdapiApplicationTests.java
+++ src/test/java/com/github/harriris/wdapi/WdapiApplicationTests.java
@@ -7,8 +7,10 @@ import com.github.harriris.wdapi.services.routes.HslRouteApiService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(properties = {"digitransit.api.key=1234"})
 class WdapiApplicationTests {
 	@Autowired
 	private DisruptionInfoController disruptionInfoController;
