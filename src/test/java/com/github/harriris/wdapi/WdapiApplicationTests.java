ffa7c114cd1ab4707bad590c9c9d6aa9f4664c8e
Harri Rissanen <=>
2023-06-06 16:31:20 +0300

    Remove unnecessary use of TestPropertySource

--- src/test/java/com/github/harriris/wdapi/WdapiApplicationTests.java
+++ src/test/java/com/github/harriris/wdapi/WdapiApplicationTests.java
@@ -7,10 +7,8 @@ import com.github.harriris.wdapi.services.routes.HslRouteApiService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.TestPropertySource;
 
-@SpringBootTest
-@TestPropertySource(properties = {"digitransit.api.key=1234"})
+@SpringBootTest(properties = {"digitransit.api.key=1234"})
 class WdapiApplicationTests {
 	@Autowired
 	private DisruptionInfoController disruptionInfoController;
